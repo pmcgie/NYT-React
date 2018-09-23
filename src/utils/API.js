b4f42f4fef5746c482907b527601d0172d792548
pmcgie <pmcgie@gmail.com>
2018-09-23 09:34:57 -0500

    structure change and API added

---
+++ src/utils/API.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export default {
+  
+  getArticles: function(topic, beginDate, endDate) {
+    
+    // API query components less dates
+    const baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    const Key = "api-key=20a42ddb30c640478da2a6b7bdf74412";
+    
+    const queryURL = baseURL + Key + "&q=" + topic + "&begin_date=" + beginDate + "0101&end_date=" + endDate + "0101";
+
+    return axios.get(queryURL);
+
+  },
+
+  saveArticle: function(newArticle) {
+    return axios.post("/api/saved", newArticle);
+  },
+  
+  getSavedArticle: function() {
+    return axios.get("/api/saved");
+  },
+  
+  deleteArticle: function(ID) {
+    return axios.delete("/api/saved/" + ID);
+  }
+
+};
+
+module.exports = API;
